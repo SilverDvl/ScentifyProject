09ae7fcf4ba411d30c0f851723f16e4b72c51e84
VarvaraGlushkova <varvara.glushkova@icloud.com>
2023-10-30 14:21:59 +0300

    updates

--- webpack.common.js
+++ webpack.common.js
@@ -134,6 +134,22 @@ module.exports = {
                 template_filename: '*',
                 priority: 'replace'
             }
+        ]),
+        new HtmlWebpackPartialsPlugin([
+            {
+                path: path.join(__dirname, './src/partials/footer.html'),
+                location: 'footer',
+                template_filename: '*',
+                priority: 'replace'
+            }
+        ]),
+        new HtmlWebpackPartialsPlugin([
+            {
+                path: path.join(__dirname, './src/partials/header.html'),
+                location: 'header',
+                template_filename: '*',
+                priority: 'replace'
+            }
         ])
     ],
     optimization: {
